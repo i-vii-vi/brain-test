dc967c1151283d7414489d6c34da7e87f04400e4
IVIIVI <150897475+i-vii-vi@users.noreply.github.com>
2025-09-08 14:33:34 +0200

    Update deye_fix.py

--- deye_fix.py
+++ deye_fix.py
@@ -986,6 +986,12 @@ except:
     print("error")
     #error_handle(imei="868373070934429", error="0")
 
+try:
+    run_data(imei="868373070934486", inverter_serial="2211176341")
+except:
+    print("error")
+    #error_handle(imei="868373070934486", error="0")
+
 print()
 
 # time.sleep(5)
